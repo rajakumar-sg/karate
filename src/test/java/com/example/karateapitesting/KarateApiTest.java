1c11d88bd7ace5788d8e1dd179fd7ede7bd110e6
raj <torajakumar@gmail.com>
2019-07-04 22:29:55 +0800

    enable mvn test

--- src/test/java/com/example/karateapitesting/KarateApiTest.java
+++ src/test/java/com/example/karateapitesting/KarateApiTest.java
@@ -1,8 +1,10 @@
 package com.example.karateapitesting;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
+@KarateOptions(features = "classpath:")
 public class KarateApiTest {
 }
